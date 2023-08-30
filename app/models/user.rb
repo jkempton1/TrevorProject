69621821da1f83ca84a227e8eb670339b17a96bb
jkempton1 <jckkempton@gmail.com>
2023-08-29 23:57:25 -0600

    updated applicationcontroller to include user persistence across the site. added 'current' model for session persistence. updated navbar to include user login / logout features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]{2,}\z/i, message: "Invalid email" }
-    validates :user_name, presence: true, uniqueness: true, format: { with: /\A[\w]{4,}\z/i, message: "Invalid username"}
+    validates :user_name, presence: true, uniqueness: true, format: { with: /\A[\w]{4,}\z/i, message: "Invalid username, must be at least 4 characters"}
     has_secure_password
 end
